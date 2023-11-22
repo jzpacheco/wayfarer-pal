26d3164e17a580dfa7f851ab3f1a9c0c8d402f20
GTM16 <146287094+GTM16@users.noreply.github.com>
2023-11-21 23:09:31 -0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,43 @@
 // src/components/Login.jsx
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
-  // Lógica de login pode ser implementada aqui
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar lógica para autenticação
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Adicione a lógica de autenticação real aqui
+  };
 
   return (
     <div className="login">
       <h2>Login</h2>
-      {/* Campos de login, formulário, etc. */}
+      <form>
+        <label>
+          Usuário:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Senha:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="button" onClick={handleLogin}>
+          Entrar
+        </button>
+      </form>
     </div>
   );
 };
